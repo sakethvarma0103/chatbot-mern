0aa018d51dbc9b412a94e2b45d584b3e1de713a3
sakethvarma0103 <vdantulu4@gitam.in>
2024-04-17 01:04:39 +0530

    frontend-setup2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
-import react from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.tsx'
+import react from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import "./index.css";
 
 
 ReactDOM.render(
