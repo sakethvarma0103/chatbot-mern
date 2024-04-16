16ba284809c530e47f0bfa76a993f699722b0811
sakethvarma0103 <vdantulu4@gitam.in>
2024-04-16 22:01:38 +0530

    login-signup

--- backend/dist/routes/user-routes.js
+++ backend/dist/routes/user-routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controller.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controller.js";
+import { validate, signupValidator, loginValidator } from "../utils/validators.js";
 const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/login", validate(loginValidator), userLogin);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/Users.js";
+import { hash,compare } from "bcrypt"; 
 
 export const getAllUsers = async (
     req: Request,
@@ -9,6 +10,55 @@ export const getAllUsers = async (
     try {
       //get all users
       const users = await User.find();
+      return res.status(200).json({ message: "OK", users });
+    } catch (error) {
+      console.log(error);
+      return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+  };
+
+  export const userSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { name, email, password } = req.body;
+      const userExisting= await User.findOne({email});
+      if(userExisting)
+        {
+          return res.status(401)
+            .json({ message: "ERROR", cause: "User already exists" });
+        }
+      const hashedPassword= await hash(password,10);
+      const users = new User({name,email,password:hashedPassword});
+      await users.save();
+      return res.status(201).json({ message: "OK", users });
+    } catch (error) {
+      console.log(error);
+      return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+  };
+
+  export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { email, password } = req.body;
+      const users = await User.findOne({email});
+      if(!users)
+        {
+          return res.status(401)
+            .json({ message: "ERROR", cause: "User does not exists" });
+        }
+      const pwcorrect=await compare(password,users.password);
+      if(!pwcorrect)
+        {
+          return res.status(403)
+            .json({ message: "ERROR", cause: "Password is incorrect" });
+        }
       return res.status(200).json({ message: "OK", users });
     } catch (error) {
       console.log(error);
