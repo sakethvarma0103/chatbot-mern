9a42e11329ab559d74b136ea21d98b6b193b71eb
sakethvarma0103 <vdantulu4@gitam.in>
2024-04-17 13:30:56 +0530

    default theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,17 @@ import "./index.css";
 import { ThemeProvider, createTheme } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 
+React.useLayoutEffect=React.useEffect;
+
+
 const theme=createTheme({
   typography:{fontFamily:"Roboto Slab,serif",allVariants:{color:"white"}}
 });
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <BrowserRouter>
   <App />
+  </BrowserRouter>
   </ThemeProvider>,
   document.getElementById('root')
 );
