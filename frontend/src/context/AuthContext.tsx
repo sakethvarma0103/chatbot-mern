6e905ac14afcf9022d6d394f0a187b2a343da9c3
sakethvarma0103 <vdantulu4@gitam.in>
2024-04-19 20:26:05 +0530

    auth context

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,40 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
 
-const AuthContext = createContext(null)
-export default AuthContext
+type User={
+    name:string;
+    email:string;
+}
+type UserAuth={
+    isLoggedIn:boolean;
+    user:User | null;
+    login:(email:string, password:string)=>Promise<void>;
+    signup:(name:string,email:string, password:string)=>Promise<void>;
+    logout:()=>Promise<void>;
+}
+const AuthContext = createContext<UserAuth | null >(null)
+const AuthProvider=({children}:{children:ReactNode})=>{
+    const [user,setUser]=useState<User | null>(null)
+    const [isLoggedIn,setIsLoggedIn]=useState(false)
+
+    useEffect(()=>{},[]);
+    const login= async (email:string, password:string)=>{
+
+    }
+    const signup= async (name:string,email:string, password:string)=>{
+
+    }
+    const logout= async ()=>{
+
+    }
+
+    const value={
+        user,
+        isLoggedIn,
+        login,
+        signup,
+        logout,
+    }
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+    
+export const useAuth=() => useContext(AuthContext)
